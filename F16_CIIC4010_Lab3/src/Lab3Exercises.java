6e731ab8145f1bcdd4d203e22bf14b1b1cef3d9a
cristianduquegonzalez <cristianduquegonzalez@10.31.20.182>
2016-08-31 13:05:01 -0400

    Draw a star

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,6 +3,8 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Cristian G. Duque Gonzalez");
+		 MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 //		myFrame.setLocation(200, 200);
 		myFrame.setSize(200, 200);
