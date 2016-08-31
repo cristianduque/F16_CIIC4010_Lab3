a59780bcc554e2aee2e9eb30c11b435fa4fdf605
cristianduquegonzalez <cristianduquegonzalez@10.31.20.182>
2016-08-31 11:54:40 -0400

    Re-sized the window and located it in the center

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Cristian G. Duque Gonzalez");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+//		myFrame.setLocation(200, 200);
+		myFrame.setSize(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
 }
